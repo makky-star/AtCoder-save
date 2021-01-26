99c93cfb44f1d14f78c8a6092f52aa60832c704b
makky_star <cannondale0430@gmail.com>
2021-01-26 17:53:00 +0900

    second commit

---
+++ ABC140/A.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cout << pow(n,3) << endl;
+    return 0;
+}
