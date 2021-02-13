b6c9e18f0f3f62121c1de855d8af2ae5ca896f5e
makky_star <cannondale0430@gmail.com>
2021-02-13 10:09:05 +0900

    2/13

--- ABC135/B.cpp
+++ ABC135/B.cpp
@@ -4,17 +4,19 @@ using namespace std;
 using ll = long long;
 
 int main(){
-    int n,max_num=-1,min_num = 100;
+    int n;
     cin >> n;
-    vector<int> p(n),q(n);
-    rep(i,n)cin >> p[i];
-    rep(i,n)q[i] = p[i];
-    sort(q.begin(),q.end()); 
-    
-    for(int i = 0;i < n;i++){
-        if(min_num > p[i])min_num = p[i];
-        if(max_num < p[i])max_num = p[i];
+    vector<int> p1(n),p2(n);
+    rep(i,n){
+        cin >> p1[i];
+        p2[i] = i+1;
     }
-    swap(min_num,max_num);
-    if()
+    int cnt = 0;
+    rep(i,n){
+        if(p1[i] == p2[i])cnt++;
+    }
+    if(cnt == n-2 || cnt == n)cout << "YES" << endl;
+    else cout << "NO" << endl;
+
+    return 0;
 }
--- ABC190/C.cpp
+++ ABC190/C.cpp
@@ -4,5 +4,33 @@ using namespace std;
 using ll = long long;
 
 int main(){
-    
+    int n,m;
+    cin >> n >> m;
+    vector<int> a(m),b(m);
+    rep(i,m)cin >> a[i] >> b[i];
+    int k;
+    cin >> k;
+    vector<int> c(k),d(k);
+    rep(i,k)cin >> c[i] >> d[i];
+    int k2 = 1 << k;
+    int ans = 0;
+    rep(s,k2){// 2^k’Ê‚èŽŽ‚·
+        vector<int> dish(n+1);
+        rep(i,k){
+            if(s>>i&1)dish[d[i]]++;
+            else dish[c[i]]++;
+
+        }
+        int now = 0;
+        rep(i,m){
+            if(dish[a[i]] == 0)continue;
+            if(dish[b[i]] == 0)continue;
+            now++;
+        }
+        ans = max(ans,now);
+    }
+    cout << ans << endl;
+
+    return 0;
+
 }
