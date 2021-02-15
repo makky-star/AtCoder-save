066743fc00f22a35c8cef0101c489187302491b5
makky_star <cannondale0430@gmail.com>
2021-02-15 22:25:42 +0900

    2/15

--- ABC122/B.cpp
+++ ABC122/B.cpp
@@ -4,5 +4,19 @@ using namespace std;
 using ll = long long;
 
 int main(){
-    
+    string s;
+    cin >> s;
+    int n = s.size();
+    int ans = 0;
+    int cnt = 0;
+    for(int i = 0;i < n;i++){
+        if(s[i]== 'A' || s[i]== 'C' || s[i]== 'G' || s[i]== 'T'){
+            cnt++;
+
+        }else{
+            cnt = 0;
+        }
+        ans = max(cnt,ans);
+    }
+    cout << ans << endl;
 }
